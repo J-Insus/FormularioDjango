bea6fadd76349acda6e800683e6588307fa129ec
Camilo <jcamilovmunoz@gmail.com>
2025-06-02 22:37:05 -0500

    Se establecio la ruta predeterminada de listar elementos

--- formulario/applications/home/urls.py
+++ formulario/applications/home/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from.import views
 urlpatterns = [
 
+    path('', views.ListarPruebaListView.as_view(), name='lista'),
+
     path('mostrar_datos/', views.ListarPruebaListView.as_view() , name='lista'),
     path('editar/<int:pk>/', views.EditarUpdateView.as_view(), name='editar'),  # vista para editar, con ID dinámico
     path('eliminar/<int:pk>/', views.EliminarDeleteView.as_view(), name='eliminar'),
